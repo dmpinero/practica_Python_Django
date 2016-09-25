0ac55a9c2f6707ff2f3216191ac0b5bfde06944d
Daniel Martinez <dmpinero@gmail.com>
2016-09-25 19:06:02 +0200

    Primer y segundo Ejercicio completos. Administrador del tercer ejercicio

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-
 from posts.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('categories', )
+    list_display = ('title', 'url_image_or_video', 'head', 'published_at')
+
+    # Detalle de un post
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'url_image_or_video', 'head', 'published_at', 'categories', 'owner'),
+            'classes': ('wide'),
+        }),
+    )
+
+admin.site.register(Post, PostAdmin)
